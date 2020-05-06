b4ef8f39a49e8ad3015dbafe2c8e151d528831c4
ishmam ali <ishmamalikhan@gmail.com>
2020-05-06 23:49:04 +0500

    index

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    
+</head>
+<body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<div class="container-fluid">
+<div class="container-fluid one" style=" background-color:black;
+            color:white;">
+<div class="container">
+<p class="pull-right">ishmamalikhan@gmail.com </p>
+</div>
+</div>
+<div class="container"><h1><font face="Centuar">Online Vegetable Shop</font></h1>
+</div>
+<div class="container">
+<div class="container" style="background:blue;color:white;">
+<div class="row">
+<div class="col-sm-12 mymanu">
+<nav class="navbar">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mystyle">
+<span class="icon-bar">----</span>
+<span class="icon-bar">----</span>
+<span class="icon-bar">----</span>
+</button>
+</div>
+<div class="collapse nav-bar collpase" id="mystyle">
+<ul class="nav navbar-nav">
+<li><a href="index.php" class="active"><font color="white">Home</font></a></li>
+<li><a href="#"><font color="white">About Us</font></a></li>
+<li><a href="#"><font color="white">Contact Us</font></a></li>
+<li><a href="#"><font color="white">Help</font></a></li>
+</ul>
+<div>
+</nav>
+</div>
+</div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-sm-2" style="background-color:black;color:white">
+<h1 align="center">Brands</h1>
+<center>
+<p><a href="dell.php" style="color:white;">Dell</a></p><br>
+<p><a href="dell.php" style="color:white;">HP</p></a><br>
+<p><a href="dell.php" style="color:white;">Samsung</p></a><br>
+<p><a href="dell.php" style="color:white;">Canon</p></a><br>
+</center>
+<h1 align="center">Catagory</h1>
+<center>
+<p><a href="dell.php" style="color:white;">Laptop</p></a><br>
+<p><a href="dell.php" style="color:white;">Mobile</p><a><br>
+<p><a href="dell.php" style="color:white;">Camera</p><a><br>
+<p><a href="dell.php" style="color:white;">Game</p></a>br>
+</center>
+</div>
+<div class="col-sm-10"><br>
+<table>
+<tr>
+<td><img src="img/b.png" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+<td><img src="img/a.jpg" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+<td><img src="img/c.jpg" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+</tr>
+<tr>
+<td><img src="img/b.png" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+<td><img src="img/a.jpg" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+<td><img src="img/c.jpg" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+</tr>
+<tr>
+<td><img src="img/b.png" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+<td><img src="img/a.jpg" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+<td><img src="img/c.jpg" width="150px" height="150px" style="margin:50px;"><a href="b.php" class="btn btn-primary">Buy</a></td>
+</tr>
+</table>
+
+
+</div>
+</div>
+<div class="container" style="background-color:silver;color:white;">
+<h1 align="center">My Ecomerce Website</h1>
+</div>
+</div>
+</div>
+</body>
+</html>
